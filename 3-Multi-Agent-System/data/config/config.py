c386cf7297d259c09ddfc2050981b575b353a516
OMID moradi <164475131+omid-moradi@users.noreply.github.com>
2025-12-16 20:27:38 +0330

    update files

--- 3-Multi-Agent-System/data/config/config.py
+++ 3-Multi-Agent-System/data/config/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[2]  # می‌شود ریشه 3-Multi-Agent-System
+BASE_DIR = Path(__file__).resolve().parents[2]  
 DATA_DIR = BASE_DIR / "data"
 INDEX_DIR = DATA_DIR / "indexes"
 
